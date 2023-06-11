8431655ba3bdc293ce71a5f6ac376fdf4bd0fa6a
Yulei-cn <116038178+Yulei-cn@users.noreply.github.com>
2023-06-12 01:02:19 +0200

    Delete myfirstapp directory

--- myfirstapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('index/', views.index, name='index'),
-    path('enseignement/', views.enseignement, name='enseignement'),
-    path('main/', views.main, name='main'),
-]
